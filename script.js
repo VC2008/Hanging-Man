13ce2fdda5fd62e882c9b59c4ef13b7847f6803b
VC2008 <526Vcolon@frhsd.com>
2025-03-20 11:52:11 -0400

    adding crucial game mechanics

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const wordList = [
     'Unfunny',
     'shenanigans',
     'hatred',
+    'psychological',
+    'exaggerated',
 ]
 
 //variable declaration
@@ -29,8 +31,10 @@ function startGame(level){
     guessedLetters = []
 
     selectedWord = getRandomWord(level) 
+    displayedWord = '_'.repeat(selectedWord.length)
 
     updateDifficultyDisplay(level)
+    updateUI()
 
     //show game area + difficulty, hide selection buttons
 
@@ -41,6 +45,8 @@ function startGame(level){
     document.getElementById('difficultyBox').classList.add('d-block')
 
     document.getElementById('difficultySelection').classList.add('d-none')
+
+    document.getElementById('letterInput').focus()
 }
 
 
@@ -49,7 +55,8 @@ function getRandomWord(level) {
     let filteredWords = wordList.filter ( word => {
         if (level === 'easy') return word.length <= 4
         if (level === 'medium') return word.length >= 5 && word.length <= 7
-        if (level === 'hard') return word.length >= 8
+        if (level === 'hard') return word.length >= 8 && word.length <= 10
+        if (level === 'random') return word.length >= 4
     })
     return filteredWords[Math.floor(Math.random()*filteredWords.length)]
 }
@@ -69,5 +76,43 @@ function updateDifficultyDisplay(level){
     }else if(level === 'hard'){
         difficultyBox.textContent = 'Hard Level'
         difficultyBox.classList.add('hard')
+    }else if(level === 'random'){
+        difficultyBox.textContent = 'Random Level'
+        difficultyBox.classList.add('random')
     }
+}
+
+function updateUI() {
+    document.getElementById('wordDisplay').textContent = displayedWord.split('').join(' ')
+}
+
+function guessLetter () {
+  let inputField = document.getElementById('letterInput')
+  let guessedLetter = inputField.value.toLowerCase()
+   
+ //check valid letters 
+ if (!guessedLetter.match(/^[a-z]$/)){
+    alert('Please enter a valid letter from a-z')
+    inputField.value = ''
+    return
+ }  
+
+ if(guessedLetters.includes(guessLetter)){
+    alert('You already guessed this letter')
+    inputField.value = ''
+    return
+ }
+
+guessedLetters.push(guessedLetter)
+
+//hangman mechanics, checking letter
+if (selectedWord.includes(guessedLetter)){
+    updateCorrectGuess(guessedLetter)
+} else {
+    updateWrongGuess(guessedLetter)
+}
+
+ inputField.value = ''
+ document.getElementById('letterInput').focus()
+
 }
